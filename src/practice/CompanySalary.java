5bdc8cbf329a0f7bd51f822b360f252930eea830
hujuanjuan674 <3331828993@qq.com>
2021-04-25 20:36:21 +0800

    hello

---
+++ src/practice/CompanySalary.java
@@ -0,0 +1,55 @@
+package practice;
+abstract class Employee{
+    public abstract double earnings();
+}
+class YearWorker extends Employee{
+    public double earnings(){
+        return 12000;
+    }
+}
+class MonthWorker extends Employee{
+    public double earnings(){
+        return 12*2300;
+    }
+}
+class WeekWorker extends Employee{
+    public double earnings(){
+        return 52*780;
+    }
+}
+class HourWorker extends Employee{
+    public double earnings() {
+        return 15*98;
+    }
+}
+class Company{
+    Employee[]employee;
+    double salaries=0;
+    Company(Employee[]employee){
+      this.employee=employee;
+    }
+    public double salariesPay(){
+        salaries=0;
+        for(int i=0;i<employee.length;i++){
+            salaries=salaries+employee[i].earnings();
+        }
+        return salaries;
+    }
+}
+public class CompanySalary {
+    public static void main(String args[]){
+        Employee[]employee=new Employee[29];
+        for(int i=0;i< employee.length;i++){
+            if(i%4==0)
+                employee[i]=new HourWorker();
+            else if(i%4==1)
+                employee[i]=new WeekWorker();
+            else if(i%4==2)
+                employee[i]=new MonthWorker();
+            else if(i%4==3)
+                employee[i]=new YearWorker();
+        }
+        Company company=new Company(employee);
+        System.out.println("公司薪水总额:"+company.salariesPay()+"元");
+    }
+}
